894924ce9e036672f3ef4a4b7358b4e537dfc6ae
Noel <terrorer9999@gmail.com>
2014-07-09 23:28:51 +0800

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TraditionalChineseWebsite::Application.config.secret_key_base = '90396426632c3f7ef2051ecf0d217592ff2bc749bd05a6aea2aeac76eb1d4b3165f42983c197b1a115d46a3a41e8d7401b59f666108ce31f8f77fa028eccefa0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TraditionalChineseWebsite::Application.config.session_store :cookie_store, key: '_traditional_chinese_website_session'
