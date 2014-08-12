e209e4e3f9e86aadbec0417f689ffdfa9636d5b0
Noel <terrorer9999@gmail.com>
2014-08-13 00:14:16 +0800

    edit for application_helper and create view/admin::sixbooks/new, edit, _form!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def collection_for_six_category
+    SixbookCategory.all.map{ |s| [s.category_name, s.id] }
+  end
+
+  def collection_for_dictionary_word
+    Dictionary.all.map{ |d| [d.word, d.id] }
+  end
 end
