eaadab04c10a6815530f6c4e0daab639559741f8
Noel <terrorer9999@gmail.com>
2014-08-14 23:52:40 +0800

    drop off show action from sixbook resources!

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ TraditionalChineseWebsite::Application.routes.draw do
   devise_for :users
   namespace :admin do
     root :to => "base#index"
-    resources :sixbooks
+    resources :sixbooks, :except => :show
   end
 end
