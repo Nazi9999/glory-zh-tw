7f6fc4ed740bea6df2f8149ad39903b54ca3d00f
Noel <terrorer9999@gmail.com>
2014-08-14 23:51:02 +0800

    mount_uplodaer :file in sixbook!

--- app/models/sixbook.rb
+++ app/models/sixbook.rb
@@ -2,4 +2,6 @@ class Sixbook < ActiveRecord::Base
   belongs_to :sixbook_category, :foreign_key => :six_cat_id
   belongs_to :dictionary, :foreign_key => :dic_id
   delegate :category_name, :to => :sixbook_category, :allow_nil => true
+
+  mount_uploader :file, SixbookFileUploader
 end
