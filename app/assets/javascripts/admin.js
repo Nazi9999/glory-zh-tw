7845765e92d1876a629ec395cdf844433923e036
Noel <terrorer9999@gmail.com>
2015-08-26 01:16:01 +0800

    設置tinymce!

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -25,6 +25,7 @@
 //= require twitter/bootstrap
 //= require unicorn
 
+//= require tinymce-jquery
 //= require_self
 
 $(document).ready(function() {
@@ -76,5 +77,25 @@ $(document).ready(function() {
       return false;
     });
   });
-  
+
+  // tinyMCE.init({
+  //   selector: 'textarea.tinymce'
+  // });
+  tinyMCE.init({
+    selector: 'textarea.tinymce',
+    plugins: ["advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visualblocks visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor colorpicker textpattern"],
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+    image_advtab: true,
+    templates: [
+      {
+        title: "Test template 1",
+        content: "Test 1"
+      }, {
+        title: "Test template 2",
+        content: "Test 2"
+      }
+    ]
+  });
+    
 });
